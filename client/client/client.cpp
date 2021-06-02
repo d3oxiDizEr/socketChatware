7a938477f334bcee3c739e3001841d7e600486ab
Hyffer <gaohuyufan@qq.com>
2021-06-02 21:19:36 +0800

    chatroom backend base edition 3
    new features: register, login, message sender and send time

--- client/client/client.cpp
+++ client/client/client.cpp
@@ -1,20 +1,75 @@
-﻿#include<winsock2.h>
-#pragma comment(lib, "WS2_32")
-#include<stdio.h>
+﻿#define PORT 5000
+
+#define M_LOGIN 1
+#define M_REGISTER 2
+#define M_PASSWORD 3
+#define M_CLOSE 4
+
+#define S_UNLOGIN 5
+#define S_AUTHERR 6
+#define S_AUTHSUC 7
+
+#include <winsock2.h>
+#pragma comment(lib, "ws2_32.lib")
+
+#include <thread>
+
+#include <stdio.h>
+
+using namespace std;
+
+
+struct userpass {
+	char c;
+	char name[20];
+	char pass[20];
+};
+
+
+struct PACKAGE {
+	tm time;//36
+	char name[20];
+	char msg[144];
+};
+
+
+void copychars(char* dst, char* src, int num) {
+	for (int i = 0; i < num; i++) {
+		dst[i] = src[i];
+	}
+}
+
 
 class Client {
 private:
+	void (*input)(char*);
+	void (*recvmsg)(tm, char*, char*);
+	void (*authin)(userpass*);
+
 	SOCKET sock;
 	SOCKADDR_IN addr, serveraddr;
 	char sendbuf[200];
-public:
-	int init(int);
-	void start();
+	char recvbuf[200];
+	PACKAGE pkgp;
+
 	void receive();
+
+public:
+	void set(void (*)(char*), void (*)(tm, char*, char*), void (*)(userpass*));
+	int init();
+	void login();
+	void start();
+	thread receive_th();
 	void stop();
 };
 
-int Client::init(int serverport) {
+void Client::set(void (*fp1)(char*), void (*fp2)(tm, char*, char*), void (*fp3)(userpass*)) {
+	input = fp1;
+	recvmsg = fp2;
+	authin = fp3;
+}
+
+int Client::init() {
 	// Initialise Winsock
 	WSADATA wsa;
 	if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0)
@@ -31,7 +86,7 @@ int Client::init(int serverport) {
 	memset(&serveraddr, 0x00, sizeof(serveraddr));
 	serveraddr.sin_family = AF_INET;
 	serveraddr.sin_addr.S_un.S_addr = inet_addr("127.0.0.1");
-	serveraddr.sin_port = htons(serverport);
+	serveraddr.sin_port = htons(PORT);
 	if (connect(sock, (SOCKADDR*)&serveraddr, sizeof(serveraddr)) < 0) {
 		return 2;
 	}
@@ -39,10 +94,19 @@ int Client::init(int serverport) {
 	return 0;
 }
 
+void Client::login() {
+	userpass kvp;
+	while (recvbuf[0] != S_AUTHSUC) {
+		(*authin)(&kvp);
+		copychars(sendbuf, (char*)&kvp, sizeof(kvp));
+		send(sock, sendbuf, 200, 0);
+		recv(sock, recvbuf, 200, 0);
+	}
+}
+
 void Client::start() {
 	while (1) {
-		printf("input>>>");
-		scanf("%s", sendbuf);
+		(*input)(sendbuf);
 		if (sendbuf[0] == '0') {
 			break;
 		}
@@ -51,18 +115,61 @@ void Client::start() {
 }
 
 void Client::receive() {
+	int code;
+	while (1) {
+		code = recv(sock, (char*)&pkgp, 200, 0);
+		if (code == SOCKET_ERROR) {
+			break;
+		}
+		(*recvmsg)(pkgp.time, pkgp.name, pkgp.msg);
+	}
+}
 
+thread Client::receive_th() {
+	return thread(&Client::receive, this);
 }
 
 void Client::stop() {
+	sendbuf[0] = M_CLOSE;
+	send(sock, sendbuf, 200, 0);
+	Sleep(100);// needed to get send() function done
 	closesocket(sock);
 	WSACleanup();
 }
 
+
+void input(char* s) {
+	scanf("%s", s);
+}
+
+void recv_msg(tm time, char* name, char* msg) {
+	printf("[%d:%d]%s: %s\n", time.tm_hour, time.tm_min, name, msg);
+}
+
+void auth(userpass* kvp) {
+	printf("login[1], register[2]: ");
+	scanf("%d", &(kvp->c));
+	printf("username: ");
+	scanf("%s", kvp->name);
+	printf("password: ");
+	scanf("%s", kvp->pass);
+}
+
 void main() {
 	Client client;
-	int code = client.init(5000);
-	printf("%d\n", code);
+	client.set(input, recv_msg, auth);
+	int code = client.init();
+	if (code != 0) {
+		printf("error code: %d.", code);
+		return;
+	}
+	printf("client on.\n");
+	client.login();
+
+	thread th = client.receive_th();
 	client.start();
+
+	
 	client.stop();
+	th.join();
 }
--- server/server/server.cpp
+++ server/server/server.cpp
@@ -1,30 +1,150 @@
-﻿#include<winsock2.h>
+﻿#define PORT 5000
+#define DATA "data.txt"
+
+#define M_LOGIN 1
+#define M_REGISTER 2
+#define M_PASSWORD 3
+#define M_CLOSE 4
+
+#define S_UNLOGIN 5
+#define S_AUTHERR 6
+#define S_AUTHSUC 7
+
+#include <winsock2.h>
 #pragma comment(lib, "ws2_32.lib")
-#include<windows.h>
-#include<stdio.h>
-#include<vector>
-#include<thread>
+
+#include <string>
+#include <vector>
+#include <thread>
+
+#include <stdio.h>
+
 using namespace std;
+
+
+struct userpass {
+	char c;
+	char name[20];
+	char pass[20];
+};
+
+
+struct PACKAGE {
+	tm time;//36
+	char name[20];
+	char msg[144];
+};
+
+
+void copychars(char* dst, char* src, int num) {
+	for (int i = 0; i < num; i++) {
+		dst[i] = src[i];
+	}
+}
+
 
 class Server {
 private:
+	int F_STOP = 0;
+
+	void (*joinmsg)(char*, int, int);
+	void (*leavemsg)(char*, int, int);
+
+	// database
+	FILE* fp;
+	int recordlen = sizeof(userpass);
+
+	void dbinit();
+	int login(char*, char*);
+	int signup(char*, char*);
+	void dbclose();
+
+	// socket
 	SOCKET sock;
 	SOCKADDR_IN addr;
 	vector<SOCKET> clientsocks;
 	vector<SOCKADDR_IN> clientaddrs;
+	vector<char*> usernames;
 	int addrlen = sizeof(addr);
 	unsigned long mode = 1;// non-blocking mode
-	//struct timeval timeout = { 0,10000 };
+	PACKAGE pkg;
 	char buffer[200];
-public:
-	int init(int);
-	void start();
+	time_t tsec;
+	tm* tstruct;
+
 	void accept1();
-	thread accept1th();
+	void receive();
+	void disconnect(int);
+
+public:
+	void set(void (*)(char*, int, int), void (*)(char*, int, int));
+	int init();
+	void start();
+	thread accept1_th();
+	thread receive_th();
 	void stop();
 };
 
-int Server::init(int port) {
+
+void Server::dbinit() {
+	if ((fp = fopen(DATA, "r+")) == NULL) {
+		fp = fopen(DATA, "w");
+		fclose(fp);
+		fp = fopen(DATA, "r+");
+	}
+}
+
+int Server::login(char* name, char* pass) {
+	char rname[21];
+	char rpass[21];
+	fseek(fp, 0, SEEK_SET);
+	while (fgets(rname, 21, fp) != NULL) {
+		if (strcmp(rname, name) == 0) {
+			// find username
+			fgets(rpass, 21, fp);
+			if (strcmp(rpass, pass) == 0) {
+				// password correct
+				return S_AUTHSUC;
+			}
+			return S_AUTHERR;
+		}
+		// skip password
+		fseek(fp, 20L, SEEK_CUR);
+	}
+	return S_AUTHERR;
+}
+
+int Server::signup(char* name, char* pass) {
+	char rname[21];
+	fseek(fp, 0, SEEK_SET);
+	while (fgets(rname, 21, fp) != NULL) {
+		if (strcmp(rname, name) == 0) {
+			return S_AUTHERR;
+		}
+		fseek(fp, 20L, SEEK_CUR);
+	}
+	// register
+	fseek(fp, 0, SEEK_END);
+	for (int i = 0; i < 20; i++) {
+		fputc(name[i], fp);
+	}
+	for (int i = 0; i < 20; i++) {
+		fputc(pass[i], fp);
+	}
+	return S_AUTHSUC;
+}
+
+void Server::dbclose() {
+	fclose(fp);
+}
+
+
+void Server::set(void(*fp1)(char*, int, int), void(*fp2)(char*, int, int)) {
+	joinmsg = fp1;
+	leavemsg = fp2;
+}
+
+int Server::init() {
 	// Initialise Winsock
 	WSADATA wsa;
 	if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0)
@@ -46,7 +166,7 @@ int Server::init(int port) {
 	memset(&addr, 0x00, sizeof(addr));
 	addr.sin_family = AF_INET;
 	addr.sin_addr.S_un.S_addr = htonl(INADDR_ANY);// any IP
-	addr.sin_port = htons(port);
+	addr.sin_port = htons(PORT);
 	if (bind(sock, (SOCKADDR*)&addr, addrlen) < 0) {
 		return 3;
 	}
@@ -54,11 +174,59 @@ int Server::init(int port) {
 	if (listen(sock, 16) < 0) {
 		return 4;
 	}
+
+	dbinit();
+
 	return 0;
 }
 
 void Server::start() {
+	int input;
+	scanf("%d", &input);
+
+}
+
+void Server::disconnect(int i) {
+	SOCKADDR_IN client_addr = clientaddrs[i];
+	char* client_ip = inet_ntoa(client_addr.sin_addr);
+	int client_port = ntohs(client_addr.sin_port);
+	clientsocks.erase(clientsocks.begin() + i);
+	clientaddrs.erase(clientaddrs.begin() + i);
+	free(usernames[i]);
+	usernames.erase(usernames.begin() + i);
+	leavemsg(client_ip, client_port, clientsocks.size());
+}
+
+void Server::accept1() {
+	SOCKET client;
+	SOCKADDR_IN clientaddr;
+	char* client_ip;
+	int client_port;
+	while (1) {
+		client = accept(sock, (SOCKADDR*)&clientaddr, &addrlen);
+		if (client == INVALID_SOCKET) {
+			Sleep(200);
+			if (F_STOP == 1) {
+				return;
+			}
+			continue;
+		}
+		client_ip = inet_ntoa(clientaddr.sin_addr);
+		client_port = ntohs(clientaddr.sin_port);
+		clientsocks.push_back(client);
+		clientaddrs.push_back(clientaddr);
+		usernames.push_back(NULL);
+		(*joinmsg)(client_ip, client_port, clientsocks.size());
+	}
+}
+
+thread Server::accept1_th() {
+	return thread(&Server::accept1, this);
+}
+
+void Server::receive() {
 	int code;
+	userpass* p;
 	SOCKADDR_IN client_addr;
 	char* client_ip;
 	int client_port;
@@ -66,65 +234,107 @@ void Server::start() {
 		for (int i = 0; i < clientsocks.size(); i++) {
 			code = recv(clientsocks[i], buffer, 200, 0);
 			if (code == 0) {
-				client_addr = clientaddrs[i];
-				client_ip = inet_ntoa(client_addr.sin_addr);
-				client_port = ntohs(client_addr.sin_port);
-				printf("%s:%d disconnected.\n", client_ip, client_port);
-				clientsocks.erase(clientsocks.begin() + i);
-				clientaddrs.erase(clientaddrs.begin() + i);
-				printf("now %d client(s) online.\n", clientsocks.size());
+				// client closed gracefully
+				// now useless for some reason
+				//disconnect(i);
 				continue;
 			}
 			else if (code == SOCKET_ERROR) {
-				// force shutdown client belong here!
-				//printf("%d\n", code);
+				// no receive
+				// notice: client forced shutdown belongs here
 				continue;
 			}
 			else {
-				printf("%s\n", buffer);
+				if (buffer[0] == M_CLOSE) {
+					disconnect(i);
+				}
+				else if (buffer[0] == M_LOGIN) {
+					p = (userpass*)buffer;
+					buffer[0] = login(p->name, p->pass);
+					if (buffer[0] == S_AUTHSUC) {
+						usernames[i] = (char*)malloc(20 * sizeof(char));
+						strcpy(usernames[i], p->name);
+					}
+					send(clientsocks[i], buffer, 200, 0);
+				}
+				else if (buffer[0] == M_REGISTER) {
+					p = (userpass*)buffer;
+					buffer[0] = signup(p->name, p->pass);
+					if (buffer[0] == S_AUTHSUC) {
+						usernames[i] = (char*)malloc(20 * sizeof(char));
+						strcpy(usernames[i], p->name);
+					}
+					send(clientsocks[i], buffer, 200, 0);
+				}
+				else if (usernames[i] == NULL) {
+					buffer[0] = S_UNLOGIN;
+					send(clientsocks[i], buffer, 200, 0);
+				}
+				else {
+					time(&tsec);
+					tstruct = gmtime(&tsec);
+					copychars((char*)&pkg.time, (char*)tstruct, 36);
+					copychars(pkg.name, usernames[i], 20);
+					copychars(pkg.msg, buffer, 144);
+					printf("%s\n", buffer);
+					for (int j = 0; j < clientsocks.size(); j++) {
+						if (usernames[j] == NULL) {
+							continue;
+						}
+						send(clientsocks[j], (char*)&pkg, 200, 0);
+					}
+				}
 			}
 		}
 		Sleep(50);
-	}
-}
-
-void Server::accept1() {
-	SOCKET client;
-	SOCKADDR_IN clientaddr;
-	while (1) {
-		client = accept(sock, (SOCKADDR*)&clientaddr, &addrlen);
-		if (client == INVALID_SOCKET) {
-			Sleep(200);
-			continue;
+		if (F_STOP == 1) {
+			return;
 		}
-		char* client_ip = inet_ntoa(clientaddr.sin_addr);
-		int client_port = ntohs(clientaddr.sin_port);
-		printf("connect from %s:%d.\n", client_ip, client_port);
-		clientsocks.push_back(client);
-		clientaddrs.push_back(clientaddr);
-		printf("now %d client(s) online.\n", clientsocks.size());
 	}
 }
 
-thread Server::accept1th() {
-	return thread(&Server::accept1, this);
+thread Server::receive_th() {
+	return thread(&Server::receive, this);
 }
 
 void Server::stop() {
+	F_STOP = 1;
+	dbclose();
+
 	for (int i = 0; i < clientsocks.size(); i++) {
-		closesocket(clientsocks[i]);
+		disconnect(i);
 	}
 	closesocket(sock);
 	WSACleanup();
 }
 
 
-void main()
-{
+void join_msg(char* client_ip, int client_port, int total) {
+	printf("connect from %s:%d.\n", client_ip, client_port);
+	printf("now %d client(s) online.\n", total);
+}
+
+void leave_msg(char* client_ip, int client_port, int total) {
+	printf("%s:%d disconnected.\n", client_ip, client_port);
+	printf("now %d client(s) online.\n", total);
+}
+
+void main() {
 	Server server;
-	int code = server.init(5000);
-	printf("%d\n", code);
-	thread th = server.accept1th();
+	server.set(join_msg, leave_msg);
+	int code = server.init();
+	if (code != 0) {
+		printf("error code: %d.", code);
+		return;
+	}
+	printf("server on.\n");
+	thread th1 = server.accept1_th();
+	thread th2 = server.receive_th();
 	server.start();
-	th.join();
+	
+	printf("server off.\n");
+	server.stop();
+
+	th1.join();
+	th2.join();
 }
